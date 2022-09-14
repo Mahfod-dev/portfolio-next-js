56e98098705cd38c57307bb44b0a3a29744942fb
Mahfod <mahfod.dev@gmail.com>
2022-09-14 21:16:28 +0200

    delete comments

--- components/Home/index.js
+++ components/Home/index.js
@@ -1,43 +1,7 @@
 import { Grid, Image, Text, Button, Container, Link } from '@nextui-org/react';
-import { useMediaQuery } from '../../utils/useMediaQuery';
-import { writeText } from '../../utils/character';
-import { useEffect, useState } from 'react';
-import { useRef } from 'react';
-import { MovingComponent } from 'react-moving-text';
 
 const HomePage = () => {
 	const text = 'Maaaa';
-	// const [placeHolder, setPlaceholder] = useState('');
-	// const index = useRef(1);
-
-	// useEffect(() => {
-	// 	const text = 'MaketheWeb';
-	// 	function tick() {
-	// 		setPlaceholder((prev) => prev + text[index.current]);
-	// 		console.log(text[index.current]);
-
-	// 		index.current++;
-	// 	}
-	// 	if (index.current < text.length - 1) {
-	// 		console.log(index.current, text.length);
-	// 		let addChar = setInterval(tick, 500);
-	// 		return () => clearInterval(addChar);
-	// 	}
-	// }, [placeHolder]);
-
-	const nexStr = text.split(' ').map((letter, index) => (
-		<MovingComponent
-			key={index}
-			type='effect3D'
-			duration='1000ms'
-			delay='index * 100ms'
-			direction='alternate'
-			timing='ease'
-			iteration='1'
-			fillMode='none'>
-			{letter}
-		</MovingComponent>
-	));
 
 	return (
 		<Grid.Container
